018b56636722f5da03a3ebac9b05f9b51e1a02be
nkrpradhan@gmail.com <nkrpradhan@gmail.com>
2022-11-20 18:15:11 +0000

    charting

--- src/components/screens/ProductDetailsScreen/index.js
+++ src/components/screens/ProductDetailsScreen/index.js
@@ -1,10 +1,29 @@
-import { Text, View, Image, Button, Alert, Modal } from "react-native";
+import {
+  Text,
+  View,
+  Image,
+  Button,
+  Alert,
+  ScrollView,
+  ActivityIndicator,
+} from "react-native";
 import { useEffect, useState } from "react";
 import { getProductDetails } from "../../../api/services/products";
 import { styles } from "./Style";
 import Icon from "react-native-vector-icons/FontAwesome";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import ModalScreen from "../ModalScreen";
+import { VictoryBar, VictoryChart, VictoryTheme } from "victory-native";
+
+const data = [
+  { day: 1, price: 1 },
+  { day: 2, price: 1.4 },
+  { day: 3, price: 1.35 },
+  { day: 4, price: 1.2 },
+  { day: 5, price: 1.2 },
+  { day: 6, price: 1.2 },
+  { day: 7, price: 1.2 },
+];
 
 export default function ProductDetails({ route, navigation }) {
   const { itemName, itemId } = route.params;
@@ -49,14 +68,37 @@ export default function ProductDetails({ route, navigation }) {
     checkSignedUser();
     getProductDetailsFn(itemId);
   }, []);
+
+  //Render views
+
+  if (loading) {
+    return (
+      <View style={styles.loadingContainer}>
+        <ActivityIndicator size="large" color="#0000ff" />
+      </View>
+    );
+  }
   return (
-    <View style={styles.container}>
+    <ScrollView style={styles.container}>
       <Image source={{ uri: product.pictureLink }} style={styles.logo} />
       <Text>{product.name}</Text>
       <Text>{product.description}</Text>
       <Text>{product.price}</Text>
-      <Text>{product.supermarket}</Text>
+      <Text
+        style={[
+          styles.title,
+          { textTransform: "capitalize" },
+          { fontWeight: "bold" },
+        ]}
+      >
+        {product.supermarket}
+      </Text>
       <Button title="Add" onPress={addItemtoList} />
-    </View>
+      <View>
+        <VictoryChart width={300} theme={VictoryTheme.material}>
+          <VictoryBar data={data} x="day" y="price" />
+        </VictoryChart>
+      </View>
+    </ScrollView>
   );
 }
