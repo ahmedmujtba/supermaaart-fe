9cde15d51c3b96bcdc85dc2258ab0ff0255057ad
tomdud9907 <tomdud9907@github.com>
2022-11-21 15:53:53 +0000

    styling

--- src/components/RegisterScreen.js
+++ src/components/RegisterScreen.js
@@ -73,6 +73,7 @@ const RegisterScreen = (props) => {
       });
   };
   if (isRegistraionSuccess) {
+    console.log(isRegistraionSuccess);
     return (
       <View
         style={{
@@ -82,7 +83,7 @@ const RegisterScreen = (props) => {
         }}
       >
         <Image
-          source={require("../../assets/iconA.png")}
+          source={{ uri: "https://i.ibb.co/Gsz2CdY/superm-AAART-1.png" }}
           style={{
             height: 150,
             resizeMode: "contain",
@@ -111,10 +112,12 @@ const RegisterScreen = (props) => {
       >
         <View style={{ alignItems: "center" }}>
           <Image
-            source={require("../../assets/iconA.png")}
+            source={{
+              uri: "https://i.ibb.co/K5GRb7d/supermaaart-HIGHRESUPDATED.png",
+            }}
             style={{
               width: "50%",
-              height: 100,
+              height: 150,
               resizeMode: "contain",
               margin: 30,
             }}
