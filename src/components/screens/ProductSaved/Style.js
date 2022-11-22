265ad87a1804c1e8dbadb52aa7305f03f1d30fa2
nkrpradhan@gmail.com <nkrpradhan@gmail.com>
2022-11-22 20:23:28 +0000

    save product

---
+++ src/components/screens/ProductSaved/Style.js
@@ -0,0 +1,48 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  container: {
+    marginBottom: 150,
+  },
+  itemContainer: {
+    flexDirection: "row",
+    marginHorizontal: 5,
+  },
+  imgContainer: {
+    marginRight: 10,
+  },
+  itemTextContainer: {
+    flex: 1,
+  },
+  itemNameText: {
+    flexDirection: "row",
+  },
+  item: {
+    flex: 1,
+    padding: 20,
+    marginVertical: 8,
+    marginHorizontal: 16,
+    // borderRadius: 5,
+    marginBottom: 10,
+  },
+  title: {
+    fontSize: 15,
+  },
+  itemImg: {
+    width: 100,
+    height: 100,
+  },
+  loadingContainer: {
+    flex: 1,
+    justifyContent: "center",
+  },
+  showMoreBtn: {
+    marginHorizontal: 26,
+    borderRadius: 10,
+  },
+  ratingsContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    marginVertical: 5,
+  },
+});
--- src/components/screens/ProductSaved/index.js
+++ src/components/screens/ProductSaved/index.js
@@ -1,26 +1,110 @@
-import { useContext, useEffect } from "react";
-import { View, Text } from "react-native";
+import { useContext, useEffect, useState } from "react";
+import {
+  View,
+  Text,
+  SafeAreaView,
+  FlatList,
+  ActivityIndicator,
+  Image,
+} from "react-native";
 import UserContext from "../../../context/UserContext";
 import { getFavoriteItem } from "../../../api/services/products";
-
+import { styles } from "./Style";
 export default function ProductsSaved() {
   const { signedUser, setSignedUser } = useContext(UserContext);
+  const [savedProducts, setSavedProducts] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
 
   const fetchFavItems = async (signedUser) => {
     const response = await getFavoriteItem(signedUser);
-    console.log("fetchFavItems--", response);
+    console.log("fetchFavItems--", response.data);
+    if (response.status === 200) {
+      setLoading(false);
+      setSavedProducts(response.data);
+    }
   };
   useEffect(() => {
     if (signedUser !== "") {
       console.log("user signed in", signedUser);
+      setLoading(true);
       fetchFavItems(signedUser);
     } else {
       console.log("please sign in");
     }
   }, []);
-  return (
+
+  const Item = ({ item, textColor }) => (
     <View>
-      <Text>Saved products list</Text>
+      <View style={styles.itemContainer}>
+        <View style={styles.imgContainer}>
+          <Image source={{ uri: item.pictureLink }} style={styles.itemImg} />
+        </View>
+        <View style={styles.itemTextContainer}>
+          <View style={styles.itemNameText}>
+            <Text
+              style={[styles.title, textColor, { flex: 1, flexWrap: "wrap" }]}
+            >
+              {item.name}
+            </Text>
+          </View>
+
+          <Text
+            style={[
+              styles.title,
+              textColor,
+              { textTransform: "capitalize" },
+              { fontWeight: "bold" },
+            ]}
+          >
+            {item.supermarket}
+          </Text>
+          <Text style={[styles.title, textColor]}>£{item.price}</Text>
+        </View>
+      </View>
     </View>
+  );
+  const renderItem = ({ item }) => {
+    const backgroundColor = "white";
+    const color = "#243578";
+
+    return (
+      <Item
+        item={item}
+        backgroundColor={{ backgroundColor }}
+        textColor={{ color }}
+      />
+    );
+  };
+  if (loading) {
+    return (
+      <View style={styles.loadingContainer}>
+        <ActivityIndicator size="large" color="#0000ff" />
+      </View>
+    );
+  }
+  if (error !== "") {
+    return (
+      <View>
+        <Text>Error in fetching data</Text>
+      </View>
+    );
+  }
+  if (signedUser === "") {
+    return (
+      <View>
+        <Text>Please sign in to see the items.</Text>
+      </View>
+    );
+  }
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <FlatList
+        data={savedProducts}
+        renderItem={renderItem}
+        keyExtractor={(item) => item._id}
+      />
+    </SafeAreaView>
   );
 }
